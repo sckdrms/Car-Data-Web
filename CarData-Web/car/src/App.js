28c51e43107caa4867265193e1afe03507792b6b
Changguen Kim <rms1359@gmail.com>
2024-03-22 01:22:32 +0900

    add components of fullpage

--- CarData-Web/car/src/App.js
+++ CarData-Web/car/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactFullpage from "@fullpage/react-fullpage";
+
 import {Navbar, Container, Nav} from 'react-bootstrap';
 import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+
+import Fullpagecomponents from "../src/components/FullpageComponents"
 
 import "./styles.css";
 const SEL = "custom-section";
@@ -13,33 +15,14 @@ function App() {
 
 
 
-  const onLeave = (origin, destination, direction) => {
-    console.log("onLeave", { origin, destination, direction });
-  };
+
 
   return (
     <div className="App">
+      <Fullpagecomponents>
+        
+      </Fullpagecomponents>
 
-      <ReactFullpage
-        debug // Debug logging
-        navigation
-        anchors={["firstPage", "secondPage", "thirdPage", "fourthPage"]}
-        sectionSelector=".section"
-        slidesNavigation={true} // Enable navigation bullets for slides
-        onLeave={onLeave}
-        render={({ state, fullpageApi }) => (
-          <ReactFullpage.Wrapper>
-            <div className="section" id="page1"><p>1 page</p></div>
-            <div className="section">
-              <div className="slide" id="page2-1"><p>page 2-1</p></div>
-              <div className="slide"><p>page 2-2</p></div>
-              <div className="slide"><p>page 2-3</p></div>
-            </div>
-            <div className="section" id="page3-1"><p>3 page</p></div>
-            <div className="section"><p>4 page</p></div>
-          </ReactFullpage.Wrapper>
-        )}
-      />
     </div>
   );
 }
